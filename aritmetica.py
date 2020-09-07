97f0a0d52c262110d0f12411f94694ccf1b7bd8b
Ignecio <ignacio.pki@gmail.com>
2020-09-07 02:38:51 -0300

    aritmetica.py

---
+++ aritmetica.py
@@ -0,0 +1,9 @@
+x=3
+y=4
+z=10
+print (f'x vale {x} e y vale {y}; por ejemplo, y+x = {y}+{x} = {y+x}')
+print (f'x vale {x} y z vale {z}; por lo tanto, x-z = {x}-{z} = {x-z}')
+print (f'y vale {y} y x vale {x}; por lo tanto, y/x = {y}/{x} = {y/x}')
+print (f'z vale {z} e y vale {y}; por lo tanto, z*y = {z}*{y} = {z*y}')
+print (f'z vale {z} y x vale {x}; por lo tanto, z%x = {z}%{x} = {z%x}')
+

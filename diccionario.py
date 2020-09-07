cb327e95ee2c2ef65430f744f5ba752654075c0f
Ignecio <ignacio.pki@gmail.com>
2020-09-07 04:15:32 -0300

    diccionario.py

--- diccionario.py
+++ diccionario.py
@@ -1,8 +1,12 @@
-diccionario= {'nombre':['Pablo', ' Andrés', ' Javiera', ' Karin'], 'apellido':['Farias',' Roman',' Lufí',' Orellana'], 'notapro':[6.0, 5.0, 7.0, 4.0], 'notafis':[5.7, 6.4, 4.4, 5.9]}
+diccionario= {'nombre':['Pablo', 'Andrés', 'Javiera', 'Karin'], 'apellido':['Farias','Roman','Lufí','Orellana'], 'notapro':[6.0, 5.0, 7.0, 4.0], 'notafis':[5.7, 6.4, 4.4, 5.9]}
 
+print ('Nombre')
 print (diccionario['nombre'][2])
+print ('Apellido')
 print (diccionario['apellido'][2])
+print ('Nota Programacion')
 print (diccionario['notapro'][2]) #notas de programación#
+print ('Nota Física')
 print (diccionario['notafis'][2]) #notas de fisica#
 
 
